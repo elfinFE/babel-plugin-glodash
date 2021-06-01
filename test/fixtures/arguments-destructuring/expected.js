ab2df8f8f33dfcf8735a550495f28080ef9da51a
hint <hintcao@xiaojing0.com>
2021-06-01 18:33:28 +0800

    add arguments-destructuring unit test

---
+++ test/fixtures/arguments-destructuring/expected.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _union2 = _interopRequireDefault(require("lodash/union"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var a = [[1], [1, 2]];
+
+_union2["default"].apply(void 0, a);
